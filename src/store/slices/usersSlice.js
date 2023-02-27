b0f9f74a4d730e6856fce170238f0eca587a9a39
tahany777 <tahanymohammed777@hotmail.com>
2023-02-27 22:11:37 +0300

    feat: the structure for components

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState: {
+        data: [],
+        isLoading: false,
+        error: null,
+    },
+    reducers: {
+
+    }
+});
+
+export const usersReducer = usersSlice.reducer;
