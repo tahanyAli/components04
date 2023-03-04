a31aac8057ab9dfd88d24fe0e5629291d168b312
tahany777 <tahanymohammed777@hotmail.com>
2023-03-04 19:14:01 +0300

    feat: add and get albums using redux toolkit query

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
 
 function AlbumsList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  }
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.is} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
   return (
-    <div>Albums for: {user.name}</div>
-  )
+    <div>
+      <div>
+      Albums for: {user.name}
+      <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
-export default AlbumsList
+export default AlbumsList;
